d2f8f61c0cfe944cb97e5e8ad0e607cc69c3b1b5
Khadim <khadimfall0602@gmail.com>
2022-06-14 23:46:10 +0200

    start to unify

---
+++ lib/models/chess_color_selection.dart
@@ -0,0 +1,22 @@
+library chess_cloud_provider;
+
+enum ChessColorSelection {
+  random,
+  white,
+  black,
+}
+
+extension ChessColorSelectionExtension on ChessColorSelection {
+
+  String get text {
+    switch (this) {
+      case ChessColorSelection.white:
+        return "white";
+      case ChessColorSelection.black:
+        return 'black';
+      default:
+        return "random";
+    }
+  }
+
+}

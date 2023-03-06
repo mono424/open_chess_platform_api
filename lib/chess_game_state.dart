e759f39d196025035c5ffab302c3a8e319253ebd
Khadim <khadimfall0602@gmail.com>
2023-03-06 01:03:38 +0100

    readonly game state

--- lib/chess_game_state.dart
+++ lib/chess_game_state.dart
@@ -6,9 +6,11 @@ import 'package:chess_cloud_provider/models/chess_color.dart';
 import 'package:chess_cloud_provider/models/chess_game_connection_state.dart';
 import 'package:chess_cloud_provider/models/game_status.dart';
 
-class ChessGameState {
-  final Stream<ChessGameState> _stream;
+class ChessGameStateIntGetter {
+  ChessGameStateInt Function()? get;
+}
 
+class ChessGameStateInt {
   // Game State
   GameStatus status = GameStatus.unknown;
   List<String> moves = [];
@@ -31,16 +33,42 @@ class ChessGameState {
   // Chat
   List<ChatMessage> chat = [];
 
-  ChessGameState(this._stream);
+  // Connection State
+  ChessGameConnectionState connectionState = ChessGameConnectionState.disconnected;
+  ChessPlatformException? connectionError;
+}
+
+class ChessGameState {
+  final Stream<ChessGameState> _stream;
+  final ChessGameStateInt _state = ChessGameStateInt();
+
+  // Getters
+  GameStatus get status => _state.status;
+  List<String> get moves => [..._state.moves];
+  Duration get whiteTime => _state.whiteTime;
+  Duration get blackTime => _state.blackTime;
+  Duration get whiteIncrement => _state.whiteIncrement;
+  Duration get blackIncrement => _state.blackIncrement;
+  bool get clockRunning => _state.clockRunning;
+  DateTime get timeBase => _state.timeBase;
+  ChessColor? get winner => _state.winner;
+  bool get whiteOfferingDraw => _state.whiteOfferingDraw;
+  bool get blackOfferingDraw => _state.blackOfferingDraw;
+  bool get whiteProposingTakeback => _state.whiteProposingTakeback;
+  bool get blackProposingTakeback => _state.blackProposingTakeback;
+  bool get opponentOnline => _state.opponentOnline;
+  DateTime get claimWinAt => _state.claimWinAt;
+  List<ChatMessage> get chat => [..._state.chat];
+  ChessGameConnectionState get connectionState => _state.connectionState;
+  ChessPlatformException? get connectionError => _state.connectionError;
+
+  ChessGameState(this._stream, getter) {
+    getter.get = () => _state;
+  }
 
   Stream<ChessGameState> get stream {
     return _stream;
   }
-
-  // Connection State
-  ChessGameConnectionState connectionState = ChessGameConnectionState.disconnected;
-  ChessPlatformException? connectionError;
-
 
   // Convinience Methods
   Duration whiteTimeLeft(bool isRunning) => isRunning ? (whiteTime - (DateTime.now().difference(timeBase))) : whiteTime;
@@ -49,9 +77,10 @@ class ChessGameState {
 
 class ChessGameStateController {
   final StreamController<ChessGameState> _streamController = StreamController();
+  final ChessGameStateIntGetter _stateGetter = ChessGameStateIntGetter();
+  late final ChessGameState _state = ChessGameState(_streamController.stream.asBroadcastStream(), _stateGetter);
 
-  late final ChessGameState state =
-      ChessGameState(_streamController.stream.asBroadcastStream());
+  ChessGameState get state => _state;
 
   ChessGameStateController();
 
@@ -68,7 +97,9 @@ class ChessGameStateController {
     required bool whiteProposingTakeback,
     required bool blackProposingTakeback,
     ChessColor? winner,
-    DateTime? timeBase }) {
+    DateTime? timeBase,
+  }) {
+    final state = _stateGetter.get!();
     state.status = status;
     state.moves = moves;
     state.whiteTime = whiteTime;
@@ -86,23 +117,26 @@ class ChessGameStateController {
   }
 
   void updateOpponentOnline(bool online, {DateTime? claimWinAt}) {
+    final state = _stateGetter.get!();
     state.opponentOnline = online;
     state.claimWinAt = claimWinAt ?? DateTime(0);
     _notify();
   }
 
   void setConnectionState(ChessGameConnectionState connectionState, { ChessPlatformException? connectionError }) {
+    final state = _stateGetter.get!();
     state.connectionState = connectionState;
     state.connectionError = connectionError;
     _notify();
   }
 
   void addChatMessage({ required String username, required String content, DateTime? createdAt }) {
+    final state = _stateGetter.get!();
     state.chat.add(ChatMessage(username: username, content: content, createdAt: createdAt ?? DateTime.now()));
     _notify();
   }
 
   void _notify() {
-    _streamController.add(state);
+    _streamController.add(_state);
   }
 }

6fdc24c0633231b0b8d80e7dcd005cb321e9fccd
Khadim <khadimfall0602@gmail.com>
2023-03-10 00:32:07 +0100

    add exception

---
+++ lib/exceptions/chess_platform_auth_in_progress.dart
@@ -0,0 +1,7 @@
+library chess_cloud_provider;
+
+import 'package:chess_cloud_provider/chess_platform_exception.dart';
+
+class ChessPlatformAuthInProgress extends ChessPlatformException {
+  ChessPlatformAuthInProgress();
+}

5b3cefe8de4e9af324111d4077ae8d7cd0300ea1
Khadim <khadimfall0602@gmail.com>
2024-06-10 13:25:27 +0200

    Add User autocomplete

---
+++ lib/platforms/lichess/models/lichess_autocomplete_result.dart
@@ -0,0 +1,41 @@
+library open_chess_platform_api;
+
+class LichessAutocompleteResult {
+  late String userId;
+  late String name;
+  late String title;
+  late bool patron;
+  late bool online;
+
+  LichessAutocompleteResult(
+      {required this.userId,
+      required this.name,
+      required this.title,
+      required this.patron,
+      required this.online,
+      });
+
+  LichessAutocompleteResult.fromJson(Map<String, dynamic> json) {
+    userId = (json['id'] != null && json['id'] is String) ? json['id'] : null;
+    name = json['name'];
+    title = json['title'];
+    online = json['patron'] ?? false;
+    online = json['online'] ?? false;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = {};
+    data['id'] = userId;
+    data['name'] = name;
+    data['title'] = title;
+    data['patron'] = patron;
+    data['online'] = online;
+    return data;
+  }
+
+  @override
+  bool operator ==(other) => other is LichessAutocompleteResult && other.userId == userId;
+
+  @override
+  int get hashCode => userId.hashCode;
+}
